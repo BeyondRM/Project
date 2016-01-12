9eeeabb42ccee842bd1b59f18052ef2d53665e5a
JustGregory <gregory.cheyney@gmail.com>
2016-01-12 00:16:28 -0600

    Added 'projects' module.
    Includes classes Project, Projects, and a localization bundle
    Projects.properties

---
+++ projects/src/main/java/brm/projects/project/Project.java
@@ -0,0 +1,11 @@
+package brm.projects.project;
+
+
+/**
+ * A Project object. This abstract class is the parent class for various BeyondRM projects; it is extended to provide a
+ * specialized implementation. This object only provides the baseline project data.
+ * @author Gregory <gregory.cheyney@gmail.com>
+ */
+abstract public class Project {
+  protected String projectName;
+}
