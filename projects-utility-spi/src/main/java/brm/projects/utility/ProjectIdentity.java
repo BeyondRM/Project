ddb2d4aa6113cd407425186ecb4fd2381b078ad7
JustGregory <gregory.cheyney@gmail.com>
2016-09-09 05:46:18 -0500

    Added bits.
    The class gets a single line of doc text, and the getProjectName() and
    setProjectName() methods can have a default body in case nothing needs
    to be obtained.

--- projects-utility-spi/src/main/java/brm/projects/utility/ProjectIdentity.java
+++ projects-utility-spi/src/main/java/brm/projects/utility/ProjectIdentity.java
@@ -2,9 +2,14 @@ package brm.projects.utility;
 
 
 /**
- *
+ * The project identity.
  * @author Gregory <gregory.cheyney@gmail.com>
  */
 public interface ProjectIdentity {
-  public String getProjectName();
+  default public String getProjectName() {
+    return null;
+  }
+
+  default public void setProjectName(String s) {
+  }
 }
