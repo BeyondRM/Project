af1dadd715ec8fce90a1d6620e0e6c383a3b2cd2
JustGregory <gregory.cheyney@gmail.com>
2016-09-09 05:41:23 -0500

    Javadocs tweaks.
    For some reason, the IDE thinks paragraph tags cannot be self-closing,
    whereas I still wanna use it like that. DARN YOU, WEB STANDARDS!!!

--- projects-utility-spi/src/main/java/brm/projects/utility/ProjectBuilder.java
+++ projects-utility-spi/src/main/java/brm/projects/utility/ProjectBuilder.java
@@ -21,13 +21,13 @@ package brm.projects.utility;
  * a pseudo-guarantee that the developer is not working with previous binary data and executables. Some developers like
  * their development-environment to keep precompiled "working copy" objects cached with their projects at any one time;
  * it may shorten or simplify some compilation and building times, but otherwise is unnecessary.
- * <p/>
+ * <p>
  * In Enterbrain's RPG Maker products, the goal is to keep the database working-copy data as binary objects, not openly
  * editable text files such as Ruby scripts and assorted Database files as XML or other interpretable markup data. This
  * is in some part due to their "protecting" their assets from being openly modified outside of their editor interfaces
  * &mdash; even though a role-playing game maker would benefit from a more open-source approach. The benefit is that it
  * is merely a matter of converting from the editor's binary format to the engine's binary format, in releasing a game.
- * <p/>
+ * <p>
  * But in contrast, BeyondRM is taking a rather unique approach in trying to maintain as much openness as is necessary,
  * for multiple reasons. Text-based files are notably easier to index and merge into a version-control repository site,
  * also making team-based game creation quite a bit more easy. Additionally, should something "go wrong" with an editor
@@ -36,7 +36,7 @@ package brm.projects.utility;
  * to use plain text and open formats....
  * <h1>When Is A Project Complete?</h1>
  * Interesting question; but it's one that "depends" &hellip; okay, a bit of programming humor, there&hellip;.
- * <p/>
+ * <p>
  * In all truth, it's not realistic to expect <i>absolute</i> perfection from a game, because we are all imperfect, and
  * bound to make occasional mistakes. But calling a project "done" without doing more than a cursory test or two may be
  * a bit premature; some players may be <b>really</b> turned off by random crashes, grammatical errors, punctuation and
@@ -44,7 +44,7 @@ package brm.projects.utility;
  * want to know <i>approximately</i> how many hours of game-play a person can expect to be entertained in a single play
  * of the game; having a general "rough estimate" for a minimum and maximum time length may help the target audience to
  * decide whether they want to take a chance on the game or not.
- * <p/>
+ * <p>
  * @author Gregory <gregory.cheyney@gmail.com>
  */
 public interface ProjectBuilder {
@@ -67,7 +67,7 @@ public interface ProjectBuilder {
   /**
    * Whether a project can be played as a game. The playing mode assumes nothing more needs to be done; the game engine
    * and assorted data should be considered complete.
-   * <p/>
+   * <p>
    * It is like {@code canProjectPlayTest()}, but without additional diagnostic or post-test information synchronized.
    * @return A {@link Boolean} condition.
    * @see ProjectBuilder
@@ -78,7 +78,7 @@ public interface ProjectBuilder {
   /**
    * Whether a project can be played as a test. The testing mode may enable certain "test mode" features, as diagnostic
    * components within the game play itself.
-   * <p/>
+   * <p>
    * It is like {@code canProjectPlayTest()}, but under certain circumstances, some test-play resulting data <i>may</i>
    * be importable back into an editor project, allowing for <i>partial</i> two-way data creation.
    * @return A {@link Boolean} condition.
@@ -89,7 +89,7 @@ public interface ProjectBuilder {
   /**
    * Whether a project data is built. A project's data is built only when all build-output is ready to be paired to the
    * engine and released as a finished, functional game, by normal means.
-   * <p/>
+   * <p>
    * In some conditions, we could use the word "finished" rather loosely, where a game-creator might "release" tests of
    * their game data and modules for others to try, while it is still not complete. Usually, this is for the purpose of
    * allowing a few players to act as "beta-testers" in order to find game-breaking bugs or statistical imbalances with
